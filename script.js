00b73ffdc0d4b832bef51488edf5ae5dc56838d5
Chicco80 <chicco80lisa@gmail.com>
2022-04-13 11:05:19 +0200

    fine prima parte

--- script.js
+++ script.js
@@ -1,28 +1,71 @@
-const colNm = 100;
-const colForRow = 10;
+/*<div class="gridrow">
+   <div class="box"><span>2</span>
+ </div>*/
 
-function printGrid(){
-let app = document.getElementById("app");
-let row = document.createElement( "div");
-row.setAttribute("class", "row");
-let cols = createCol();
-// console.log(cols);
-row.innerHTML = cols;
-app.append(row);
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+
+
+ 
+  function setLevel(event){
+    //   console.log(this);
+
+   const lvl = document.getElementById("lvl").value;
+   console.log(lvl);
+   let numQuad;
+   switch (lvl){
+        case "1":
+        default:
+           numQuad = 100;
+           break;
+        case "2":
+            numQuad = 81;
+            break;
+        case "3":
+            numQuad = 49;
+            break;   
+   }
+    let boxPerSide = Math.sqrt(numQuad);
+    console.log(boxPerSide)
+    genGrid(numQuad, boxPerSide);
+  }
+
+  function genGrid(numQuad, boxPerSide){
+  console.log(numQuad)
+  let app = document.getElementById("app");
+  app.innerHTML = "";
+  let row = document.createElement("div");
+  row.setAttribute("class","gridrow");
+  for(let i = 1; i<= numQuad; i++){
+  const quad = genBox(i,boxPerSide);
+  row.append(quad);
+  }
+  app.append(row);
 
 }
-
-function createCol(){
-    let cols = "";
-    for(let i = 1; i <= colNm; i++){
-        // console.log(i);
-        cols += `
-        <div class="col col-4 box">
-          ${i} 
-         </div>`;
-        
-    }
-    return cols;
+function genBox(numQuad,boxPerSide){
+    let quad = document.createElement("div");
+    quad.setAttribute("class", "box");
+    quad.style.width = `calc(100% / ${boxPerSide})`;
+    quad.style.height = `calc(100% / ${boxPerSide})`;
+    quad.classList.add("puntatore")
+    quad.innerHTML = `<span>${numQuad}</span>`
+    quad.addEventListener("click", coloraCella);
+    return quad;
 
 }
-printGrid();
+function coloraCella(){
+  this.style.backgroundColor = "#6495ed";
+  this.classList.remove("puntatore");
+  this.removeEventListener("click", coloraCella);
+}
+
+ document.getElementById("play").addEventListener("click", setLevel);
+
+ //prima parte finita//
+
